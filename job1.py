730b9b3520184c0ec4fc3d908f012e958a15075c
ruchizz <ruchitham1912@gmail.com>
2026-07-01 13:33:18 +0530

    Update job1.py

--- job1.py
+++ job1.py
@@ -1,3 +1,5 @@
-import pandas as pd
-from jobspy import scrape_jobs
-from datetime import datetime
+if __name__ == "__main__":
+    hours_since_last = get_last_run_hours()
+    jobs_df = get_jobs(hours_since_last)
+    save_to_excel(jobs_df)
+    save_current_run_time()
